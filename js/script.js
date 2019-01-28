20ab61e3cf77b44465088c58626ad87125ddc9a4
Jesse <junjiejesse@gmail.com>
2019-01-28 01:07:19 +0000

    semi-finished

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 /* global $ */
 
-// BELOW Update the songs array with four of your favorites songs.
-let i=0;
+//let i=0;
 let songName = ["BBiBBi", "Lemon", "You(=I)", "告白", "九万字", "Palette", "Loser"];
 let linkUrl = ["https://youtu.be/nM0xDI5R50E","https://youtu.be/SX_ViT4Ra7k", "https://youtu.be/oKalxTZwoyY", "https://youtu.be/n6RG8UuAJLg", "https://youtu.be/u7S2-_Uk_9s","https://youtu.be/d9IxdwEFk1c", "https://youtu.be/Dx_fKPBPYUI" ];
 let lengthTime = ["3:28", "4:34", "2:50", "4:19", "3:52", "3:38", "4:02"];
@@ -13,7 +12,7 @@ let imageUrl = ["http://i.epochtimes.com/assets/uploads/2018/09/1809140454341007
                 "https://i.ytimg.com/vi/_nBiUtS55k0/hqdefault.jpg",
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQcujjEbjMa3waDZ8tHAUEwIsS41QROhjSXHrZXXLF2WaqhHwy",
                 "https://mykindofjapan.files.wordpress.com/2017/03/2662b4d7-s.jpg?w=660"];
-let playButton= ["Play", "play", "Play", "Play", "Play", "Play", "Play"];            
+//let playButton= ["Play", "play", "Play", "Play", "Play", "Play", "Play"];            
 
 displaySongInfo();
 
@@ -23,11 +22,11 @@ $("#add").click(function() {
     displaySongInfo();
 });
 
-$("#a"+i).clcik(function() {
+/*$("#a"+i).clcik(function() {
     $("#a"+i).hide();
     $("#links").empty();
     $("#links").html(linkUrl[i]);
-});
+});*/
 
 function emptySongInfo(){
     $(".empty").val("");
@@ -43,14 +42,17 @@ function addSongInfo(){
     
     if(song==="" || link==="" || length==="" || artist==="" || image==="") {
         alert("Please Complete The Song Info");
-    }else{ 
+    }/*else if(length.isNaN()) {
+        alert("Please enter a valid number for time");
+    }else if(!image.includes("http") || !link.includes("http")) {
+        alert("Please enter a valid url");
+    }*/else{ 
         songName.push(song);
         linkUrl.push(link);
         lengthTime.push(length);
         artistName.push(artist);  
-        playButton.push("Play");
+        //playButton.push("Play");
         imageUrl.push(image);
-      
     }
 }
 
@@ -59,10 +61,10 @@ function displaySongInfo() {
         $("#songs").append(`<p>${name}</p>`);
     });
 
-    playButton.forEach(function(button) {
+    /*playButton.forEach(function(button) {
         $("#plays").append("<p id=a"+i+">"+button+"</p>");
         i++;
-    });
+    });*/
 
     lengthTime.forEach(function(time) {
         $("#lengths").append(`<p>${time}</p>`);
@@ -77,8 +79,8 @@ function displaySongInfo() {
     }); 
     
     linkUrl.forEach(function(url) {
-        $("#links").append(`<iframe src=' ${url} '>`);
-    })
+        $("#links").append(` <a href="${url}"> <button class="i"> > </button> </a> `);
+    });
 }
 
 
